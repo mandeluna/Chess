5b59b4d7eb8b0cd1146bac625a70955387f0b210
Steven Wart <steve.wart@gmail.com>
2025-09-28 08:40:25 -0700

    Changed app icon

---
+++ CaptureView.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+import PlaygroundSupport
+
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 320, height: 480))
+view.backgroundColor = .white
+
+PlaygroundPage.current.liveView = view
